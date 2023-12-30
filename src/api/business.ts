4735aa63bd6ca0e9f3f50fbade054737f0a98b37
Ahmad Purnomo <purnomo.ahmad97@gmail.com>
2023-12-30 13:32:29 +0700

    feat: implement proxy

--- src/api/business.ts
+++ src/api/business.ts
@@ -1,12 +1,10 @@
 import type { IPayloadGetBusinessList } from './request.interface'
 import type { ICategory, IDataBusiness, IResponse } from './response.interface'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL
-
 export const getBusinessList = async (
   payload: IPayloadGetBusinessList
 ): Promise<IResponse<IDataBusiness>> => {
-  const res = await fetch(`${BASE_URL}/business/parent/all`, {
+  const res = await fetch(`/api/business/parent/all`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload)
@@ -15,6 +13,6 @@ export const getBusinessList = async (
 }
 
 export const getBusinessCategories = async (): Promise<IResponse<ICategory[]>> => {
-  const res = await fetch(`${BASE_URL}/media/param/business/category`)
+  const res = await fetch(`/api/media/param/business/category`)
   return await res.json()
 }
